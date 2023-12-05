8151723d58ebe573d71b79c6ef46d1bea504200e
cozyroom12 <wjddms6841@gmail.com>
2023-12-05 17:23:02 +0900

    add final w/o styles

---
+++ react-movie/MD/react-for-beginners/src/routes/Home.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            // 여기서 props인 coverImg 이름은 아무렇게나 지어도 됨. Movie 컴포넌트에 임포트시에 이름만 맞춰주면 댐
+            <Movie
+              // 여기도 key를 넣어줘야 함? ㅇㅋ 왜지?
+              // for when you're rendering components inside the map function !!! super important.
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
